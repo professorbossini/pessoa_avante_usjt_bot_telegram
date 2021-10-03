18462f52f5de396b3ee9f59fe3e2bd6071bedb66
Rodrigo Bossini <professorbossini@gmail.com>
2021-10-02 22:48:16 -0300

    Adjustments. Commands now are /top10 and /ra 'ra'. There is no text manipulation

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const { default: axios } = require("axios");
 const dotenv = require("dotenv");
 dotenv.config();
-const { Telegraf, Markup } = require("telegraf");
-const { stringify } = require ('flatted')
+const { Telegraf } = require("telegraf");
+const { stringify } = require ('flatted');
 
 const bot = new Telegraf(process.env.TOKEN);
 
-const startMessage = "Sou o bot Avante USJT! Consulte já a sua pontuação. /help para mais detalhes";
-const helpMessage = "Digite seu ra para ver sua posição. Digite /top10 para ver os 10 primeiros";
+const startMessage = "Sou o bot Avante USJT! Consulte já a sua pontuação. /help para mais detalhes.";
+const helpMessage = "I. /ra 'seura' sem as aspas para ver a sua posição.\nII. /top10 para ver o top 10.";
 const settingsMessage = "No settings at the moment";
 const sorryMessage = "Desculpe, ainda não sei nada sobre isso.";
 
@@ -15,38 +15,65 @@ bot.start((ctx) => ctx.reply(startMessage));
 bot.help((ctx) => ctx.reply(helpMessage));
 bot.settings((ctx) => ctx.reply(settingsMessage));
 
-
-bot.command('top10', async (ctx) => {
+const resolveTop10 = async (ctx, resolve) => {
     try{
         const respostaAxios = await axios.get(`https://ancient-cove-06766.herokuapp.com/top_ones`)
-        console.log ("Resposta Axios: " + stringify(respostaAxios))
+        // console.log ("Resposta Axios: " + stringify(respostaAxios))
         let texto = '';
         const mapeado = respostaAxios.data.map((e, posicao) =>{
-            return `${posicao + 1}\u00B0||${e.nome}\n`
+            return `${posicao + 1}\u00B0. ${e.nome.split(' ').slice(0, 2).join(' ')} tem ${e.avtCoins} avtcoins.\n`
         })
         mapeado.forEach(e => texto += e)
-        const respostaTelegram = await ctx.reply(texto);
-        console.log ("Resposta Telegram: " + stringify(respostaTelegram))
+        const respostaTelegram = await resolve (texto);
+        // console.log ("Resposta Telegram: " + stringify(respostaTelegram))
     }
     catch (e){
         console.log(e)
-        ctx.reply('Problemas técnicos. Tente novamente mais tarde')
+        resolve('Problemas técnicos. Tente novamente mais tarde')
     }
-})
-bot.on("text", async (ctx) => {
-  try {
-    const ra = ctx.message.text;
-    const respostaAxios = await axios.get(`https://ancient-cove-06766.herokuapp.com/myself_among_others?ra=${ra}`)
-    console.log ("Resposta Axios: " + stringify(respostaAxios.data))
-    const aluno = respostaAxios.data.find(aluno => aluno.ra === ra)
-   
-    const respostaTelegram = await ctx.reply(`Olá, ${aluno.nome.trim()}. Sua posição é ${aluno.posicao}\u00B0. Você tem ${aluno.avtCoins} avtcoins.`);
-    console.log ("Resposta Telegram: " + stringify(respostaTelegram))
-  } catch (err) {
-    console.log(err);
-    ctx.reply('RA não encontrado. Consulte o suporte.');
+}
+
+const resolveRA = async (ctx, resolve) => {
+    try {
+      const ra = ctx.message.text.split(' ')[1];
+      const respostaAxios = await axios.get(`https://ancient-cove-06766.herokuapp.com/myself_among_others?ra=${ra}`)
+    //   console.log ("Resposta Axios: " + stringify(respostaAxios.data))
+      const aluno = respostaAxios.data.find(aluno => aluno.ra === ra)
+     
+      const respostaTelegram = await resolve(`Olá, ${aluno.nome.trim()}. Sua posição é ${aluno.posicao}\u00B0. Você tem ${aluno.avtCoins} avtcoins.`);
+    //   console.log ("Resposta Telegram: " + stringify(respostaTelegram))
+    } catch (err) {
+      console.log(err);
+      resolve('RA não encontrado. Consulte o suporte.')
+    //   ctx.reply();
+    }
   }
-});
+
+
+// bot.on ('inline_query', async (ctx) => {
+//     if (ctx.inlineQuery.query === '/top10'){
+//         resolveTop10(ctx, (resposta) => ctx.answerInlineQuery(resposta), ctx.inlineQuery.query)
+//     }
+//     else{
+//         resolveRA(ctx, (resposta) => ctx.answerInlineQuery(resposta), ctx.inlineQuery.query)
+//     }
+// })
+
+
+
+
+
+bot.command('top10', async (ctx) => resolveTop10 (ctx, (resposta) => ctx.reply(resposta)))
+bot.command('ra', async (ctx) => resolveRA (ctx, (resposta) => ctx.reply(resposta)))
+// bot.on("text", async (ctx) => {
+//     const texto = ctx.message.text
+//     if (texto.includes('top10')){
+//         resolveTop10 (ctx, (resposta) => ctx.reply(resposta))
+//     }
+//     else if (texto.includes ('ra')){
+//         resolveRA (ctx, (resposta) => ctx.reply(resposta))
+//     }
+// })
 
 
 bot.launch();
