4db29c93944aa419acae33edbb68423cc58d31d5
Rodrigo Bossini <professorbossini@gmail.com>
2021-10-19 01:00:45 -0300

    News text adjusted as per Leia's recommendations

--- index.js
+++ index.js
@@ -62,10 +62,8 @@ const resolveRA = async (ctx, resolve) => {
 // })
 
 
-
-
 const textoNews = `
-  19/10/2021:\nAvtcoins pelo questionário (Leia)\nAvtcoins pelas presenças em aula até o dia 13/10\nAvtcoins pelos puzzles (Iara)\nAvtcoins sobre a prova de tópicos diversos (Bossini)`
+  19/10/2021:\nAvtcoins pela trilha de perspectivas e questionário do estudante (em andamento)\nAvtcoins pelas presenças em aula até o dia 13/10\nAvtcoins pelos puzzles - trilha de vídeos (atualização finalizada)\nAvtcoins da prova de tópicos gerais cujo prazo final era 13/10 (atualização finalizada)`
 bot.command('top100', async (ctx) => resolveTop10 (ctx, (resposta) => ctx.reply(resposta)))
 bot.command('avtcoins', async (ctx) => resolveRA (ctx, (resposta) => ctx.reply(resposta)))
 bot.command('enade', async (ctx) => ctx.reply ('/enade: Conheça mais sobre a prova aqui: http://enade.inep.gov.br/enade/'))
